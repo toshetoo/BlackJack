b428cf09072734452948df4d871d6d0ba1cb5255
toshetoo <toshko.1987@abv.bg>
2015-03-26 20:38:31 +0200

    files aded

---
+++ BlackJack/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHold = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.lblDealer = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPot = new System.Windows.Forms.Label();
+            this.lblPlayerScore = new System.Windows.Forms.Label();
+            this.lblDealerScore = new System.Windows.Forms.Label();
+            this.lblMoneyValue = new System.Windows.Forms.Label();
+            this.lblMoney = new System.Windows.Forms.Label();
+            this.btnBetTen = new System.Windows.Forms.Button();
+            this.btnBetFifty = new System.Windows.Forms.Button();
+            this.lblPlayerCards = new System.Windows.Forms.Label();
+            this.lblDealerCards = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHold
+            // 
+            this.btnHold.Location = new System.Drawing.Point(197, 227);
+            this.btnHold.Name = "btnHold";
+            this.btnHold.Size = new System.Drawing.Size(75, 23);
+            this.btnHold.TabIndex = 0;
+            this.btnHold.Text = "Hold";
+            this.btnHold.UseVisualStyleBackColor = true;
+            this.btnHold.Click += new System.EventHandler(this.btnHold_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(197, 198);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // lblDealer
+            // 
+            this.lblDealer.AutoSize = true;
+            this.lblDealer.Location = new System.Drawing.Point(12, 9);
+            this.lblDealer.Name = "lblDealer";
+            this.lblDealer.Size = new System.Drawing.Size(38, 13);
+            this.lblDealer.TabIndex = 2;
+            this.lblDealer.Text = "Dealer";
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Location = new System.Drawing.Point(12, 160);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(36, 13);
+            this.lblPlayer.TabIndex = 3;
+            this.lblPlayer.Text = "Player";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pot";
+            // 
+            // lblPot
+            // 
+            this.lblPot.AutoSize = true;
+            this.lblPot.Location = new System.Drawing.Point(223, 25);
+            this.lblPot.Name = "lblPot";
+            this.lblPot.Size = new System.Drawing.Size(13, 13);
+            this.lblPot.TabIndex = 5;
+            this.lblPot.Text = "0";
+            // 
+            // lblPlayerScore
+            // 
+            this.lblPlayerScore.AutoSize = true;
+            this.lblPlayerScore.Location = new System.Drawing.Point(54, 160);
+            this.lblPlayerScore.Name = "lblPlayerScore";
+            this.lblPlayerScore.Size = new System.Drawing.Size(35, 13);
+            this.lblPlayerScore.TabIndex = 6;
+            this.lblPlayerScore.Text = "label2";
+            // 
+            // lblDealerScore
+            // 
+            this.lblDealerScore.AutoSize = true;
+            this.lblDealerScore.Location = new System.Drawing.Point(56, 9);
+            this.lblDealerScore.Name = "lblDealerScore";
+            this.lblDealerScore.Size = new System.Drawing.Size(35, 13);
+            this.lblDealerScore.TabIndex = 7;
+            this.lblDealerScore.Text = "label2";
+            // 
+            // lblMoneyValue
+            // 
+            this.lblMoneyValue.AutoSize = true;
+            this.lblMoneyValue.Location = new System.Drawing.Point(243, 97);
+            this.lblMoneyValue.Name = "lblMoneyValue";
+            this.lblMoneyValue.Size = new System.Drawing.Size(29, 13);
+            this.lblMoneyValue.TabIndex = 9;
+            this.lblMoneyValue.Text = "label";
+            this.lblMoneyValue.Click += new System.EventHandler(this.lblMoneyValue_Click);
+            // 
+            // lblMoney
+            // 
+            this.lblMoney.AutoSize = true;
+            this.lblMoney.Location = new System.Drawing.Point(198, 97);
+            this.lblMoney.Name = "lblMoney";
+            this.lblMoney.Size = new System.Drawing.Size(39, 13);
+            this.lblMoney.TabIndex = 8;
+            this.lblMoney.Text = "Money";
+            // 
+            // btnBetTen
+            // 
+            this.btnBetTen.Location = new System.Drawing.Point(197, 123);
+            this.btnBetTen.Name = "btnBetTen";
+            this.btnBetTen.Size = new System.Drawing.Size(75, 23);
+            this.btnBetTen.TabIndex = 10;
+            this.btnBetTen.Text = "Bet 10";
+            this.btnBetTen.UseVisualStyleBackColor = true;
+            this.btnBetTen.Click += new System.EventHandler(this.btnBetTen_Click);
+            // 
+            // btnBetFifty
+            // 
+            this.btnBetFifty.Location = new System.Drawing.Point(197, 152);
+            this.btnBetFifty.Name = "btnBetFifty";
+            this.btnBetFifty.Size = new System.Drawing.Size(75, 23);
+            this.btnBetFifty.TabIndex = 11;
+            this.btnBetFifty.Text = "Bet 50";
+            this.btnBetFifty.UseVisualStyleBackColor = true;
+            this.btnBetFifty.Click += new System.EventHandler(this.btnBetFifty_Click);
+            // 
+            // lblPlayerCards
+            // 
+            this.lblPlayerCards.AutoSize = true;
+            this.lblPlayerCards.Location = new System.Drawing.Point(12, 198);
+            this.lblPlayerCards.Name = "lblPlayerCards";
+            this.lblPlayerCards.Size = new System.Drawing.Size(0, 13);
+            this.lblPlayerCards.TabIndex = 12;
+            // 
+            // lblDealerCards
+            // 
+            this.lblDealerCards.AutoSize = true;
+            this.lblDealerCards.Location = new System.Drawing.Point(12, 48);
+            this.lblDealerCards.Name = "lblDealerCards";
+            this.lblDealerCards.Size = new System.Drawing.Size(0, 13);
+            this.lblDealerCards.TabIndex = 13;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(197, 59);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 14;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblDealerCards);
+            this.Controls.Add(this.lblPlayerCards);
+            this.Controls.Add(this.btnBetFifty);
+            this.Controls.Add(this.btnBetTen);
+            this.Controls.Add(this.lblMoneyValue);
+            this.Controls.Add(this.lblMoney);
+            this.Controls.Add(this.lblDealerScore);
+            this.Controls.Add(this.lblPlayerScore);
+            this.Controls.Add(this.lblPot);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.lblDealer);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.btnHold);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHold;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label lblDealer;
+        private System.Windows.Forms.Label lblPlayer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPot;
+        private System.Windows.Forms.Label lblPlayerScore;
+        private System.Windows.Forms.Label lblDealerScore;
+        private System.Windows.Forms.Label lblMoneyValue;
+        private System.Windows.Forms.Label lblMoney;
+        private System.Windows.Forms.Button btnBetTen;
+        private System.Windows.Forms.Button btnBetFifty;
+        private System.Windows.Forms.Label lblPlayerCards;
+        private System.Windows.Forms.Label lblDealerCards;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ BlackJack/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        static int money = 500;
+        static int playerScore = 0;
+        static int dealerScore = 0;
+        static int pot = 0;
+        static int[] cards = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        static string[] faces = { "J", "Q", "K", "A" };
+        Random rnd = new Random();
+
+        private void PlayerWin()
+        {
+            MessageBox.Show("Game ended. Player won!");
+            money += pot * 2;
+            pot = 0;
+            playerScore = 0;
+            dealerScore = 0;
+            lblMoneyValue.Text = money.ToString();
+            lblPot.Text = "";
+            lblPlayerScore.Text = "";
+            lblDealerScore.Text = "";
+            lblDealerCards.Text = "";
+            lblPlayerCards.Text = "";
+            btnDraw.Visible = false;
+            btnHold.Visible = false;
+            btnBetFifty.Enabled = true;
+            btnBetTen.Enabled = true;
+        }
+        private void DealerWin()
+        {
+            MessageBox.Show("Game ended. Dealer won!");
+            pot = 0;
+            playerScore = 0;
+            dealerScore = 0;
+            lblPot.Text = "";
+            lblPlayerScore.Text = "";
+            lblDealerScore.Text = "";
+            lblDealerCards.Text = "";
+            lblPlayerCards.Text = "";
+            btnDraw.Visible = false;
+            btnHold.Visible = false;
+            btnBetFifty.Enabled = true;
+            btnBetTen.Enabled = true;
+        }
+        private void PlayerFaces(int pIndexFaces)
+        {
+            if (faces[pIndexFaces] == "A")
+            {
+                playerScore += 11;
+                lblPlayerScore.Text = playerScore.ToString();
+            }
+            else
+            {
+                playerScore += 10;
+                lblPlayerScore.Text = playerScore.ToString();
+            }
+        }
+        private void DealerFaces(int dFaceIndex)
+        {
+            if (faces[dFaceIndex] == "A")
+            {
+                dealerScore += 11;
+                lblDealerScore.Text = dealerScore.ToString();
+            }
+            else
+            {
+                dealerScore += 10;
+                lblDealerScore.Text = dealerScore.ToString();
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblMoneyValue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDraw.Visible = false;
+            btnHold.Visible = false;
+            btnStart.Visible = false;
+            lblMoneyValue.Text = money.ToString();
+            lblPlayerScore.Text = playerScore.ToString();
+            lblDealerScore.Text = dealerScore.ToString();
+        }
+
+        private void btnBetTen_Click(object sender, EventArgs e)
+        {
+            money -= 10;
+            pot += 10;
+            lblMoneyValue.Text = money.ToString();
+            lblPot.Text = pot.ToString();
+            btnStart.Visible = true;
+            
+        }
+
+        private void btnBetFifty_Click(object sender, EventArgs e)
+        {
+            money -= 50;
+            pot += 50;
+            lblMoneyValue.Text = money.ToString();
+            lblPot.Text = pot.ToString();
+            btnStart.Visible = true;
+            
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnBetTen.Enabled = false;
+            btnBetFifty.Enabled = false;
+            int firstNum = 0;
+            int secondNum = 0;
+            firstNum = rnd.Next(0, 300);
+            secondNum = rnd.Next(0, 300);
+
+            //MessageBox.Show("f: " + firstNum + " s: " + secondNum + " ");
+
+            int pIndexCards = rnd.Next(0, cards.Length);
+            int pSecondCard = rnd.Next(0, cards.Length);
+
+            int indexCards = rnd.Next(0, cards.Length);
+            int secondCard = rnd.Next(0, cards.Length);
+
+            int pIndexFaces = rnd.Next(0, faces.Length);
+            int pSecondFaces = rnd.Next(0, faces.Length);
+            int indexFaces = rnd.Next(0, faces.Length);
+            int secondFaces = rnd.Next(0, faces.Length);
+
+            if (firstNum > secondNum)
+            {
+                lblDealerCards.Text += cards[indexCards].ToString() + " " + cards[secondCard].ToString();
+                lblPlayerCards.Text += cards[pIndexCards].ToString() + " " + cards[pSecondCard].ToString();
+                playerScore += cards[pIndexCards] + cards[pSecondCard];
+                dealerScore += cards[indexCards] + cards[secondCard];
+                lblDealerScore.Text = dealerScore.ToString();
+                lblPlayerScore.Text = playerScore.ToString();
+
+            }
+            else if (firstNum == secondNum)
+            {
+                lblDealerCards.Text += faces[indexFaces].ToString() + " " + faces[secondFaces].ToString();
+                lblPlayerCards.Text += faces[pIndexFaces].ToString() + " " + faces[pSecondFaces].ToString();
+
+                DealerFaces(indexFaces);
+                DealerFaces(secondFaces);
+
+
+                PlayerFaces(pIndexFaces);
+                PlayerFaces(pSecondFaces);
+
+
+            }
+            else
+            {
+                lblDealerCards.Text += cards[indexCards].ToString() + " " + faces[indexFaces].ToString();
+                lblPlayerCards.Text += cards[pIndexCards].ToString() + " " + faces[pIndexFaces].ToString();
+
+                playerScore += cards[pIndexCards];
+                dealerScore += cards[indexCards];
+
+                DealerFaces(indexFaces);
+                PlayerFaces(pIndexFaces);
+
+            }
+
+            if (playerScore == 21)
+            {
+                PlayerWin();
+            }
+            else if (dealerScore == 21)
+            {
+                DealerWin();
+            }
+
+            btnDraw.Visible = true;
+            btnHold.Visible = true;
+            btnStart.Visible = false;
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            int pCardIndex = rnd.Next(0, cards.Length);
+            int dCardIndex = rnd.Next(0, cards.Length);
+            int pFaceIndex = rnd.Next(0, faces.Length);
+            int dFaceIndex = rnd.Next(0, faces.Length);
+
+            int firstNum = 0;
+            int secondNum = 0;
+            firstNum = rnd.Next(0, 300);
+            secondNum = rnd.Next(0, 300);
+
+           // MessageBox.Show("f: " + firstNum + " s: " + secondNum + " ");
+            if (dealerScore == 19 || dealerScore == 20)
+            {
+                if (firstNum >= secondNum)
+                {
+                    lblPlayerCards.Text += " " + cards[pCardIndex].ToString();
+                    Thread.Sleep(1500);
+                    playerScore += cards[pCardIndex];
+                    lblPlayerScore.Text = playerScore.ToString();
+                    
+                }
+                else
+                {
+                    lblPlayerCards.Text += " " + faces[pFaceIndex].ToString();
+                    Thread.Sleep(1500);                                        
+                    PlayerFaces(pFaceIndex);
+
+                }
+            }
+            else
+            {
+                if (firstNum >= secondNum)
+                {
+                    lblPlayerCards.Text += " " + cards[pCardIndex].ToString();
+                    Thread.Sleep(1500);
+                    lblDealerCards.Text += " " + cards[dCardIndex].ToString();
+                    playerScore += cards[pCardIndex];
+                    lblPlayerScore.Text = playerScore.ToString();
+                    dealerScore += cards[dCardIndex];
+                    lblDealerScore.Text = dealerScore.ToString();
+                }
+                else
+                {
+                    lblPlayerCards.Text += " " + faces[pFaceIndex].ToString();
+                    Thread.Sleep(1500);
+                    lblDealerCards.Text += " " + faces[dFaceIndex].ToString();
+
+                    DealerFaces(dFaceIndex);
+                    PlayerFaces(pFaceIndex);
+
+                }
+            }
+
+            if (playerScore == dealerScore)
+            {
+                Console.WriteLine("Equal points. No winner!");
+                money += pot;
+                pot = 0;
+                lblMoneyValue.Text = money.ToString();
+                lblDealerCards.Text = "";
+                lblDealerScore.Text = "";
+                lblPlayerCards.Text = "";
+                lblPlayerScore.Text = "";
+                btnDraw.Visible = false;
+                btnHold.Visible = false;
+                btnBetFifty.Enabled = true;
+                btnBetTen.Enabled = true;
+            }
+            else if (playerScore == 21 && dealerScore!=21)
+            {
+                PlayerWin();
+            }
+            else if (dealerScore == 21 && playerScore!=21)
+            {
+                DealerWin();
+            }
+            if (playerScore > 21)
+            {
+                DealerWin();
+
+            }
+            else if (dealerScore > 21)
+            {
+                PlayerWin();
+            }
+        }
+        private void btnHold_Click(object sender, EventArgs e)
+        {
+
+            if ((dealerScore > playerScore) && dealerScore < 21)
+            {
+                DealerWin();
+            }
+            else if (playerScore == dealerScore)
+            {
+                Console.WriteLine("Equal points. No winner!");
+                money += pot;
+                pot = 0;
+                lblMoneyValue.Text = money.ToString();
+                lblDealerCards.Text = "";
+                lblDealerScore.Text = "";
+                lblPlayerCards.Text = "";
+                lblPlayerScore.Text = "";
+                btnDraw.Visible = false;
+                btnHold.Visible = false;
+                btnBetFifty.Enabled = true;
+                btnBetTen.Enabled = true;
+            }
+            else if (dealerScore > 21)
+            {
+                PlayerWin();
+            }
+            else
+            {
+                int dCardIndex = rnd.Next(0, cards.Length);
+                int dFaceIndex = rnd.Next(0, faces.Length);
+
+                int firstNum = 0;
+                int secondNum = 0;
+                firstNum = rnd.Next(0, 300);
+                secondNum = rnd.Next(0, 300);
+
+               // MessageBox.Show("f: " + firstNum + " s: " + secondNum + " ");
+
+                if (firstNum >= secondNum)
+                {
+
+                    Thread.Sleep(1500);
+                    lblDealerCards.Text += " " + cards[dCardIndex].ToString();
+                    dealerScore += cards[dCardIndex];
+                    lblDealerScore.Text = dealerScore.ToString();
+                }
+                else
+                {
+                    Thread.Sleep(1500);
+                    lblDealerCards.Text += " " + faces[dFaceIndex].ToString();
+
+                    DealerFaces(dFaceIndex);
+                    if (dealerScore > 21)
+                    {
+                        PlayerWin();
+                    }
+                }
+                if (dealerScore > 21)
+                {
+                    PlayerWin();
+                }
+            }
+        }
+     }
+}
+
+
